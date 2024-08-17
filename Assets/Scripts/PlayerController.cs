6580c2ae1bc9cce2a8199551a5016ee744e7d070
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2024-08-16 23:05:33 -0700

    Checkpoints

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 	// Editor variables
 	public float jumpStrength;
 	public float moveSpeed;
-
+	public Vector2 respawnPoint;
 
 	bool isGrounded;
 	Transform transformRef;
@@ -20,12 +20,17 @@ public class PlayerController : MonoBehaviour
 		transformRef = transform;
 		collider = GetComponent<Collider2D>();
 		rigidbodyRef = GetComponent<Rigidbody2D>();
+		if (respawnPoint == null) {
+			respawnPoint = new Vector2(0, 0);
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
 		updateIsGrounded();
+
+		rigidbodyRef.velocity = new Vector2(0, rigidbodyRef.velocity.y);
 
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			rigidbodyRef.velocity = new Vector2(-moveSpeed, rigidbodyRef.velocity.y);
@@ -40,9 +45,18 @@ public class PlayerController : MonoBehaviour
 		}
     }
 
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.layer == 8) {
+			rigidbodyRef.velocity = Vector2.zero;
+			transform.SetPositionAndRotation(new Vector3(respawnPoint.x, respawnPoint.y, 0), Quaternion.identity);
+		}
+	}
 
 	void updateIsGrounded() {
-
 		isGrounded = Physics2D.Raycast(transformRef.position, Vector2.down, collider.bounds.extents.y + 0.1f).collider != null;
+	}
+
+	public void updateRespawnPoint(Vector2 newLocation) {
+		respawnPoint = newLocation;
 	}
 }
